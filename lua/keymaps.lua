26c06e665d7b66aa61ffb3e2a2819f363f614496
Yirsi <admin@yirsi.com>
2024-02-29 23:10:14 +0100

    added multiple useful keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,3 @@
-
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -21,10 +20,47 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
---
 --  See `:help wincmd` for a list of all window commands
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Move selected text up or down
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
+
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- Copy to system clipboard
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]], { desc = 'Copy to system clipboard' })
+vim.keymap.set('n', '<leader>Y', [["+Y]], { desc = 'Copy line to system clipboard' })
+-- Paste from system clipboard
+vim.keymap.set({ 'n' }, '<leader>p', [["+p]], { desc = 'Paste from system clipboard' })
+
+-- paste over selected word but keep register (and send pasted over word into the void)
+vim.keymap.set('x', '<leader>p', [["_dP]], { desc = 'replace but keep register' })
+
+-- Make cursor stay in place when using J to append next line
+vim.keymap.set('n', 'J', 'mzJ`z')
+
+-- Make navigaion in normal mode less disorienting
+-- Center cursor when skipping pages
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+-- Center selection while going through pattern search
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- Opens the netrw file exporer in the current window
+vim.keymap.set('n', '<leader>.', vim.cmd.Ex, { desc = 'Open file explorer' })
+
+-- Disable Q to prevent accidental fuckups
+vim.keymap.set('n', 'Q', '<nop>')
+
+-- Edit every occurence of word under cursor using substitute
+vim.keymap.set('n', '<leader>su', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Edit every occurence of word under cursor' })
+
+-- Format current buffer
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = 'Format current buffer' })
+
+-- Make current file executable
+-- vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
